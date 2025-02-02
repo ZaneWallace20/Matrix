d22caa61e46f29b121bd40b6c2953c78ff4e85a1
ZaneWallace20 <138349317+ZaneWallace20@users.noreply.github.com>
2025-02-01 23:28:13 -0600

    Formatting

--- transitive_closure.py
+++ transitive_closure.py
@@ -1,38 +1,53 @@
-import time
-import copy
+import time 
+import random
 
+# a bad matrix class D:
 class TrnMtx:
-    matrix = [
-    [0,0,1,0],
-    [1,0,0,0],
-    [1,0,1,0],
-    [0,1,0,1],
-    ]
+
+    matrix = [[]]
+
+    def __init__(self):
+        self.random_matrix()
 
     MATRIX_LEN = len(matrix[0])
 
     def convert_to_pairs():
         array = []
 
-        for i in range(MATRIX_LEN):
-            for j in range(MATRIX_LEN):
+        for i in range(TrnMtx.MATRIX_LEN):
+            for j in range(TrnMtx.MATRIX_LEN):
                 if matrix[i][j] == 1:
                     array.append([i+1,j+1])
 
         return array
 
     def convert_from_pairs(pairs):
-        new_matrix = [[0] * MATRIX_LEN for _ in range(MATRIX_LEN)] 
+        new_matrix = [[0] * TrnMtx.MATRIX_LEN for _ in range(TrnMtx.MATRIX_LEN)] 
         
         for i in pairs:
  
             new_matrix[i[0]-1][i[1]-1] = 1
 
         return new_matrix
+    def random_matrix():
+        
+        TrnMtx.MATRIX_LEN = random.randint(3,50)
+        new_matrix = [[0] * TrnMtx.MATRIX_LEN for _ in range(TrnMtx.MATRIX_LEN)]
 
-matrix = TrnMtx.matrix
+        for i in range(TrnMtx.MATRIX_LEN):
+            for c in range(TrnMtx.MATRIX_LEN):
+                if random.randint(1,15) == 1:
+                    new_matrix[i][c] = 1
 
+        TrnMtx.matrix = new_matrix
+        return new_matrix
+
+
+temp_matrix = TrnMtx.random_matrix()
 MATRIX_LEN = TrnMtx.MATRIX_LEN
+
+matrix = temp_matrix
+
 
 # Warshall's
 
@@ -47,12 +62,13 @@ for k in range(MATRIX_LEN):
 for i in matrix:
     print(i)
 warsh_time = time.time()-t
+
+
 # calculate time
 print(warsh_time)
 
 # finds conections by swapping [s,v] -> [v+1,s]
 def find_connections(start, value):
-    print("CALL")
     # if [start,value] is a combo add it
     if [start,value] in start_array:
 
@@ -77,9 +93,6 @@ array = find_connections(1,1)
 matrix = TrnMtx.convert_from_pairs(start_array)
 for i in matrix:
     print(i)
-
-
-
 
 my_time = time.time()-t
 print(my_time)
